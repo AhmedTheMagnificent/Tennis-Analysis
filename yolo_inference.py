445f30be69024c4b0a92afaf196c488733fe11ef
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-11-13 01:23:39 +0500

    added stuff

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,4 +2,9 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8x")
 
-model.predict(r"A:\ProgrmmingStuff\Tennis-Analysis\input_videos\image.png", save=True)
+result = model.predict(r"A:\ProgrmmingStuff\Tennis-Analysis\input_videos\input_video.mp4", save=True)
+
+print(result)
+print("Boxes: ")
+for box in result[0].boxes:
+    print(box)
