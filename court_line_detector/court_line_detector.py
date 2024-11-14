1a7112f523871c7c39b4faf229e42fc3f26f81a8
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-11-15 00:42:37 +0500

    added

---
+++ court_line_detector/court_line_detector.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+import cv2 as cv
+
+class CourtLineDetector:
+    def __init__(self, model_path):
+        self.model = models.resnet50(weights=None)
+        self.model.fc = torch.nn.Linear(self.model.fc.in_features, 14*2)
+        self.model.load_state_dict(torch.load(model_path, map_location='cpu'))
+        self.transforms = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        
+    def predict(self, image):
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image = self.transforms(image).unsqueeze(0)
+        with torch.no_grad():
+            outputs = self.model(image)
+        keypoints = outputs.squeeze().cpu().numpy()
+        original_height, original_width = image.shape[:2]
+        keypoints[::2] *= original_width / 224.0
+        keypoints[1::2] *= original_height / 224.0
+        
+        return keypoints
+    
+    def draw_keypoints(self, image, keypoints):
+        for i in range(0, len(keypoints), 2):
+            x, y = int(keypoints[i]), int(keypoints[i + 1])
+            print(f"Drawing keypoint {i // 2} at ({x}, {y})")  # Debug print
+            cv.putText(image, str(i // 2), (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv.circle(image, (x, y), 5, (0, 100, 255), -1)
+        return image
+
+    
+    def draw_keypoints_on_video(self, video_frames, keypoints):
+        output_frames = []
+        for frame in video_frames:
+            frame = self.draw_keypoints(frame, keypoints)
+            output_frames.append(frame)
+        return output_frames
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from utils import (
     save_video
 )
 from trackers import PlayerTracker, BallTracker
-
+from court_line_detector import CourtLineDetector
+import cv2 as cv
 
 def main():
     input_path = r"A:\ProgrmmingStuff\Tennis-Analysis\input_videos\input_video.mp4"
@@ -11,11 +12,19 @@ def main():
     
     player_tracker = PlayerTracker(model_path=r"A:\ProgrmmingStuff\Tennis-Analysis\yolov8x.pt")
     ball_tracker = BallTracker(model_path=r"A:\ProgrmmingStuff\Tennis-Analysis\models\last.pt")
+    
     player_detections = player_tracker.detect_frames(video_frames, read_from_stubs=True, stub_path=r"A:\ProgrmmingStuff\Tennis-Analysis\tracker_stubs\player_detections.pkl")
     ball_detections = ball_tracker.detect_frames(video_frames, read_from_stubs=True, stub_path=r"A:\ProgrmmingStuff\Tennis-Analysis\tracker_stubs\ball_detections.pkl")
     
+    court_model_path = r"A:\ProgrmmingStuff\Tennis-Analysis\models\keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+    
     output_frames = player_tracker.draw_bounding_boxes(video_frames, player_detections)
-    output_frames = ball_tracker.draw_bounding_boxes(video_frames, ball_detections)
+    output_frames = ball_tracker.draw_bounding_boxes(output_frames, ball_detections)
+    
+    print(court_keypoints)
+    output_frames = court_line_detector.draw_keypoints_on_video(output_frames, court_keypoints)
     
     save_video(output_frames, r"A:\ProgrmmingStuff\Tennis-Analysis\output_videos\output_video.avi")
     
